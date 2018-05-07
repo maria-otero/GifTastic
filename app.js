a5ed8384638e3496782ded9a921bc65f0b7a3a12
M Jesús Otero <mjesuotero@Maria-Jesus-Otero-R-T.local>
2018-05-07 02:46:33 -0700

    still and animation gif ready

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 var actorsArray = ['Scarlett Johansson', 'James Franco', 'Leonardo Di Caprio'];
 
 
+var actorImg;
+
+
 // use ajax to grab gifs of actors
 function displayMovieInfo() {
     
@@ -16,20 +19,41 @@ function displayMovieInfo() {
         method: 'GET'
     })
     .then(function(response) {
-        console.log(response.data[0]);
         for(var i = 0; i < response.data.length; i++) {
             var actorDiv = $('<div>');
             var h6 = $('<h6>').text('Rating: ' + response.data[i].rating);
-            var actorImg = $('<img>');
-
-            actorImg.attr('src', response.data[i].images.fixed_height.url);
-            actorImg.addClass('');
+            
+            actorImg = $('<img>');
+            actorImg.attr('src', response.data[i].images.fixed_height_still
+            .url);
+            actorImg.attr('data-state', 'still');
+            actorImg.attr('data-still', response.data[i].images.fixed_height_still
+            .url);
+            actorImg.attr('data-animate', response.data[i].images.fixed_height.url);
             actorDiv.append(actorImg);
             actorDiv.append(h6);
             $('#gifs-area').prepend(actorDiv);
-        }
+          
+        // If the user clicks one of the still GIPHY images, the gif animate. If the user clicks the gif again, it stop playing.
+        $(actorImg).on('click', function() {
+        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+        var state = $(this).attr('data-state');
+        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+        if (state === 'still') {
+            // Then, set the image's data-state to animate
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+            // Else set src to the data-still value  
+            } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }     
     });
+    }
+});
 };
+
+
 
 // Creates buttoms
 function renderButtons() {
@@ -46,7 +70,8 @@ function renderButtons() {
 
         $('#buttons-area').append(newButton);
     } 
-}
+};
+
 
 // This function handles events when add actor button is clicked
 $('#add-actor-button').on('click', function(event) {
@@ -57,7 +82,10 @@ $('#add-actor-button').on('click', function(event) {
     // Adding actor from the textbox to our array
     actorsArray.push(actorAdded);
     renderButtons();
-})
+});
+
+
+
 
 // Adding a click event listener to all elements with a class of "actor-button"
 $(document).on('click', '.actor-button', displayMovieInfo);
